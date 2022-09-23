7da988ef870f04f25792f1e1b6e6ce87e2195054
Gaspard Jankowiak <gaspard@math.janko.fr>
2022-09-23 11:52:41 +0200

    store config, allow loading for xinit

--- flow.jl
+++ flow.jl
@@ -1,4 +1,5 @@
 using ModulatedCurves
+import HDF5
 
 func_defs_fn = "function_definitions.jl"
 default_func_defs_fn = string(func_defs_fn, ".default")
@@ -17,10 +18,30 @@ if !isfile(config_fn)
     cp(default_config_fn, config_fn)
 end
 
-function main()
-    P, S, Xinit, solver_params, options = ModulatedCurves.parse_configuration(config_fn, function_defs)
+function main(configuration_fn::String=config_fn)
+    P, S, Xinit, solver_params, options = ModulatedCurves.parse_configuration(configuration_fn, function_defs)
+
+    # HACK
+    options_dict = Dict(options)
+
+    if haskey(options_dict, :do_load)
+        h5 = HDF5.h5open(options_dict[:load_snapshot_fn], "r")
+        Xinit .= read(h5["s$(options_dict[:load_snapshot_n])X"])
+        close(h5)
+    end
+
+    if haskey(options_dict, :output_dir)
+        output_dir = options_dict[:output_dir]
+        mkpath(output_dir)
+        cp(configuration_fn, joinpath(output_dir, "config.toml"), force=true)
+        cp(func_defs_fn, joinpath(output_dir, "functions_defs.toml"), force=true)
+    end
+
+    # check differentials
+    # check_differential(P, S, Xinit)
+    # return
 
     do_flow(P, S, Xinit, solver_params; options...)
 end
 
-main()
+# main()
